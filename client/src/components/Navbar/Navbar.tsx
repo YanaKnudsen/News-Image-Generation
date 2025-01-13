3f08f018ad3cf094fdcaba93979018076fd99bfc
yana hryhorchyk <yana.hryhorchyk@gmail.com>
2025-01-13 16:44:43 +0100

    creating project

---
+++ client/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,10 @@
+"use client"
+import Link from "next/link";
+
+export default function Navbar() {
+    return (
+        <nav className="flex flex-row justify-end items-center px-5 sticky top-0 border-b h-14 bg-background">
+                    Language toggle
+        </nav>
+    );
+}
